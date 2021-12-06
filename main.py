63a6ff7af241dda82e4a4f7cc22c0a87485a834d
maximilian.fehrentz@web.de <maximilian.fehrentz@web.de>
2021-12-06 16:04:39 +0100

    some minor changes to my parts of the tuning; be careful, right now the tuning for multinomial logit model is running

--- main.py
+++ main.py
@@ -62,26 +62,26 @@ def compare_three_methods(digits, labels):
     training_digits = scale(training_digits)
     test_digits = scale(test_digits)
 
-    # # multinomial logit model
-    # clf = get_grid_multinomial_logit_model()
-    # mlm_model = clf.fit(training_digits, training_labels)
-    # print(f"The best parameters for the multinomial logit model are {mlm_model.best_params_}")
-    # mlm_pred = mlm_model.predict(test_digits)
-    # evaluate_results(model = "Multinomial Logit Model", pred = mlm_pred, actual = test_labels)
+    # multinomial logit model
+    clf = get_grid_multinomial_logit_model()
+    mlm_model = clf.fit(scale(training_digits), training_labels)
+    print(f"The best parameters for the multinomial logit model are {mlm_model.best_params_}")
+    mlm_pred = mlm_model.predict(test_digits)
+    evaluate_results(model = "Multinomial Logit Model", pred = mlm_pred, actual = test_labels)
 
     # # support vector machine
     # clf = get_grid_svm()
-    # svm = clf.fit(training_digits, training_labels)
+    # svm = clf.fit(scale(training_digits), training_labels)
     # print(f"The best parameters for the svm are {svm.best_params_}")
     # svm_pred = svm.predict(test_digits)
     # evaluate_results(model = "SVM", pred = svm_pred, actual = test_labels)
 
-    # feed-forward neural network
-    clf = get_grid_nn()
-    nn = clf.fit(training_digits, training_labels)
-    print(f"The best parameters for the nn are {nn.best_params_}")
-    nn_pred = nn.predict(test_digits)
-    evaluate_results(model = "Neural Network", pred = nn_pred, actual = test_labels)
+    # # feed-forward neural network
+    # clf = get_grid_nn()
+    # nn = clf.fit(scale(training_digits), training_labels)
+    # print(f"The best parameters for the nn are {nn.best_params_}")
+    # nn_pred = nn.predict(test_digits)
+    # evaluate_results(model = "Neural Network", pred = nn_pred, actual = test_labels)
 
 def draw_random_training_sample(digits, labels, size):
     training_indices = np.random.choice(np.arange(0, digits.shape[0]), size = size, replace = False)
@@ -99,7 +99,7 @@ def get_grid_multinomial_logit_model():
     param_grid = [
         {
             'penalty': ['l2'],
-            'C': list(np.arange(0.1, 1.1, 0.2)),
+            'C': [.001 ,.01, .1, 1, 10, 100, 1000],
             'solver': ['lbfgs'],
             'max_iter': [1000], # as long as theres no errors it's fine
             'multi_class': ['multinomial'],
@@ -111,10 +111,10 @@ def get_grid_multinomial_logit_model():
 def get_grid_svm():
     param_grid = [
         {
-            'C': [0.1,1, 10, 100], 
+            'C': [.001 ,.01, .1, 1, 10, 100, 1000], 
             # 'gamma': [1,0.1,0.01,0.001],
             'kernel': ['rbf', 'poly', 'sigmoid']
-            }
+        }
     ]
     return GridSearchCV(estimator = SVC(), param_grid = param_grid, n_jobs = -1, cv = 10, verbose =  3, scoring = 'accuracy')
 
